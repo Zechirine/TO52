9732fc7d2762bcfc1990a44b3b21396c4406262a
Zechirine <znouassi@gmail.com>
2020-04-29 02:52:18 +0200

    first commit

---
+++ MaBanque/src/test/java/com/to52/MaBanqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.to52;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaBanqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
